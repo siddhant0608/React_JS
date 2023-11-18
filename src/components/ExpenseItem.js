162668364db3b0c6a37f3b50d2c79081c422d01e
siddhant0608 <siddhantlahane66>
2023-11-18 12:27:36 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+
+function ExpenseItem(){
+    return(
+        <div>
+            <h2>Expense item!</h2>
+            <h2>Food Rs 10</h2>
+            <h2>Petrol Rs 100</h2>
+            <h2>Movies Rs 200</h2>
+        </div>
+
+    ) ;
+}
+
+export default ExpenseItem;
