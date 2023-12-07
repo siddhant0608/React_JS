702b654eabf43322332267583c5ae4e07ef9c599
siddhant0608 <siddhantlahane66>
2023-12-07 14:42:37 +0530

    Rendering Expenses with performance optimisations

--- src/App.js
+++ src/App.js
@@ -1,47 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpenses/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+    LocationOfExpenditure: "pune",
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+    LocationOfExpenditure: "hyderabad",
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+    LocationOfExpenditure: "mumbai",
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+    LocationOfExpenditure: "chennai",
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      LocationOfExpenditure: "pune",
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-      LocationOfExpenditure: "hyderabad",
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      LocationOfExpenditure: "mumbai",
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      LocationOfExpenditure: "chennai",
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const expenseItems = [];
   const addExpenseHandler = (expense) => {
-    const ExpenseData = {
-      ...expense,
-      id: Math.random().toString(),
-    };
-    console.log(ExpenseData);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   for (let i = 0; i < expenses.length; i++) {
